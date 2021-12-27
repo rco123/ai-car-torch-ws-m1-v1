f8b5e7c806546abd26219ad8e3af737cb6579413
rco123 <sgkim123456@daum.net>
2021-12-27 14:56:36 +0100

    add

---
+++ pyapp/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/bkio123/lane-det-cnn.git
+git clone https://github.com/bkio123/lane-det-r18.git
+git clone https://github.com/bkio123/traffic-light-det.git
+git clone https://github.com/bkio123/traffic-light-mark.git
+git clone https://github.com/bkio123/traffic-sign-det.git
+git clone https://github.com/bkio123/traffic-sign-mark.git
